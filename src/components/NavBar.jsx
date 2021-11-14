4fc7e64692a3f1e3ae28297bdafad7fa928369f1
Liz <78451440+lizmery@users.noreply.github.com>
2021-11-14 12:59:05 -0800

    Update NavBar.jsx
    Add toggle function for mobile view

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,24 @@
-import { Navbar, NavbarBrand, Button } from "reactstrap"
+import React, { useState } from 'react';
+import {
+    Navbar, 
+    NavbarBrand, 
+    NavbarToggler, 
+    Collapse, 
+    Nav, 
+    NavItem, 
+    NavLink, 
+    Button } from 'reactstrap';
 import { Link } from "react-router-dom";
 
 import './NavBar.css';
 
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
+    
     return (
         <Navbar
             expand="md"
@@ -13,11 +28,24 @@ const NavBar = () => {
             <NavbarBrand href="/">
                 <h3>SUPERBLOOM</h3>
             </NavbarBrand>
-            <Link to="/login">
-                <Button className="button-class" outline>
-                    Login
-                </Button>
-            </Link>
+            <NavbarToggler onClick={toggle} />
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav
+                        className="me-auto"
+                        navbar
+                    >
+                        <NavItem>
+                        <NavLink href="/">
+                            About
+                        </NavLink>
+                        </NavItem>
+                    </Nav>
+                    <Link to="/login">
+                        <Button className="button-class" outline>
+                            Login
+                        </Button>
+                    </Link>
+                </Collapse>
         </Navbar>
     )
 }
