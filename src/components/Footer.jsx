4ac00b10f7fd62a763b22c2f68adaf53c9f9d14d
lizmery <l.vigil096@gmail.com>
2021-11-14 14:58:33 -0800

    Upload Footer.jsx and Footer.css

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom';
+import {Navbar, NavbarBrand} from 'reactstrap';
+import {TiSocialInstagram, TiSocialFacebook, TiSocialTwitter} from 'react-icons/ti';
+
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <Navbar fixed="bottom" className="footer">
+            <NavbarBrand href="/">
+                <h3>SUPERBLOOM</h3>
+            </NavbarBrand>
+            <div>
+                <Link to="/">
+                    <TiSocialInstagram className="social-media" />
+                </Link>
+                <Link to="/">
+                    <TiSocialFacebook className="social-media" />
+                </Link>
+                <Link to="/">
+                    <TiSocialTwitter className="social-media" />
+                </Link>
+            </div>
+        </Navbar>
+    );
+};
+
+export default Footer;
