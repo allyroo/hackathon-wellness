c2e71c45d9dde4b11ab04453884ae52e5b9ef473
allyroo <allyse.arucan@gmail.com>
2021-11-18 12:30:56 -0700

    support

--- src/pages/DashboardPages/Mind/MindSupport.jsx
+++ src/pages/DashboardPages/Mind/MindSupport.jsx
@@ -1,25 +1,205 @@
 import NavBar from "../../../components/NavBar";
-
-import { Nav } from 'react-bootstrap';
-import { Button } from 'reactstrap';
-
-import './MindSupport.css'
+import MindResourcesDirectory from "../../../components/MindComponents/MindResourcesDirectory";
+import { Nav } from "react-bootstrap";
+import {
+  Card,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  CardText,
+  Button,
+} from "reactstrap";
+import { Dropdown, Form, Row, Col } from "react-bootstrap";
+import Footer from "../../../components/Footer";
+import sample_provider from "../../../components/images/sample_provider.png";
+import "./MindSupport.css";
 import DashboardNav from "../../../components/DashboardElements/DashboardNav";
 
+const pathname = window.location.pathname;
+
 const MindSupport = () => {
-    return (
-        <div>
-            <NavBar />
-            <div className="dashboard-main">
-                <DashboardNav />
+  return (
+    <div>
+      <NavBar />
+      <div className="dashboard-main">
+        <DashboardNav />
 
-                {/* Main Content */}
-                <div className="dashboard-content-support">
-                    <h2>from support page</h2>
-                </div>
+        {/* Main Content */}
+        <div className="dashboard-content-support">
+          <div className="parent">
+            <div className="div1">
+              <div>
+                <Card>
+                  <CardBody>
+                    <CardTitle tag="h5">
+                      Chatting with Dr. Karen Nguyen
+                    </CardTitle>
+                    <CardSubtitle className="mb-2 text-muted" tag="h6">
+                      Time left: 56min
+                    </CardSubtitle>
+                    <div className="chat-box">
+                      <CardText>
+                        <div className="therapist">
+                          Hi Sophie! How has your week been?
+                        </div>
+                        <div className="user">It's been a bit rough. </div>
+                        <div className="therapist">
+                          I'm sorry to hear about that. What has been troubling
+                          you most?
+                        </div>
+                        <div className="user">
+                          I feel uncertain about everything and I feel stuck.
+                          Like I have no direction.
+                        </div>
+                        <div className="therapist">
+                          When we don't feel confident in the path we're on,
+                          it's normal to feel the way you do. Tell me a bit
+                          more.
+                        </div>
+                      </CardText>
+                    </div>
+                    <input
+                      type="text"
+                      id="fname"
+                      name="fname"
+                      value="My parents have always had high expectations of me and I have no sense of direction because I don't know who I am."
+                    />
+                    <Button>Send</Button>
+                  </CardBody>
+                </Card>
+              </div>
             </div>
-        </div>
-    )
-}
+            <div className="div2">
+              <img src={sample_provider} alt="" />
+            </div>
+            {/* Type of therapy */}
 
-export default MindSupport
+            <div className="div3">
+              <div className="d-flex justify-content-center p-5">
+                <Form>
+                  <Row className="align-items-center">
+                    <Col xs="auto" className="my-1">
+                      <Dropdown>
+                        <Dropdown.Toggle
+                          variant="secondary"
+                          className="pillar-dropdown-toggle"
+                          id="dropdown-basic"
+                        >
+                          {pathname.includes("Type")
+                            ? "Female"
+                            : pathname.includes("female")
+                            ? "Male"
+                            : pathname.includes("male")
+                            ? "Non-binary"
+                            : "Type"}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                          <Dropdown.Item
+                          //   className="pillar-dropdown-menu"
+                          //   href="/dashboard/mind"
+                          >
+                            Coach
+                          </Dropdown.Item>
+                          <Dropdown.Item
+                          //   className="pillar-dropdown-menu"
+                          //   href="/dashboard/fitness"
+                          >
+                            Mentor
+                          </Dropdown.Item>
+                          <Dropdown.Item
+                          //   className="pillar-dropdown-menu"
+                          //   href="/dashboard/rest"
+                          >
+                            Therapist
+                          </Dropdown.Item>
+                        </Dropdown.Menu>
+                      </Dropdown>
+                      <Form.Label
+                        className="me-sm-2"
+                        htmlFor="inlineFormCustomSelect"
+                        visuallyHidden
+                      >
+                        Preference
+                      </Form.Label>
+                      <Form.Select
+                        className="me-sm-2"
+                        id="inlineFormCustomSelect"
+                      >
+                        <option value="0">Gender</option>
+                        <option value="1">Female</option>
+                        <option value="2">Male</option>
+                        <option value="3">Non-binary</option>
+                      </Form.Select>
+                      <Form.Label
+                        className="me-sm-2"
+                        htmlFor="inlineFormCustomSelect"
+                        visuallyHidden
+                      >
+                        Preference
+                      </Form.Label>
+                      <Form.Select
+                        className="me-sm-2"
+                        id="inlineFormCustomSelect"
+                      >
+                        <option value="0">Ethnicity Preferred</option>
+                        <option value="1">Black and African American</option>
+                        <option value="2">Asian</option>
+                        <option value="3">Hispanic and Latino</option>
+                        <option value="4">Native American</option>
+                        <option value="5">Pacific Islander</option>
+                      </Form.Select>
+                      <Form.Select
+                        className="me-sm-2"
+                        id="inlineFormCustomSelect"
+                      >
+                        <option value="0">Sexuality</option>
+                        <option value="1">LGBTQ</option>
+                        <option value="2">Lesbian</option>
+                        <option value="3">Bisexual</option>
+                      </Form.Select>
+                      <Form.Select
+                        className="me-sm-2"
+                        id="inlineFormCustomSelect"
+                      >
+                        <option value="0">Language</option>
+                        <option value="1">Spanish</option>
+                        <option value="1">Chinese</option>
+                        <option value="2">French</option>
+                        <option value="3">German</option>
+                        <option value="3">Arabic</option>
+                        <option value="3">ASL</option>
+                      </Form.Select>
+                      <Form.Select
+                        className="me-sm-2"
+                        id="inlineFormCustomSelect"
+                      >
+                        <option value="0">Faith</option>
+                        <option value="1">Buddhist</option>
+                        <option value="2">Hindu</option>
+                        <option value="3">Islam</option>
+                        <option value="3">Jewish</option>
+                      </Form.Select>
+                    </Col>
+                    <Col xs="auto" className="my-1">
+                      <Form.Check
+                        type="checkbox"
+                        id="customControlAutosizing"
+                        label="Remember my preference"
+                      />
+                    </Col>
+                    <Col xs="auto" className="my-1">
+                      <Button type="submit">Submit</Button>
+                    </Col>
+                  </Row>
+                </Form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default MindSupport;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -9,7 +9,9 @@ import community from "../components/images/community.svg";
 import tracker from "../components/images/tracker.svg";
 import support from "../components/images/support.svg";
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 import { BsFlower1 } from "react-icons/cg";
+import { NavLink, Button } from "reactstrap";
 import "./Landing.css";
 
 const Landing = () => {
@@ -26,6 +28,11 @@ const Landing = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </div>
+            <NavLink href="/about">
+              <Button className="button-class" outline>
+                Learn More
+              </Button>
+            </NavLink>
           </div>
         </section>
         {/* breaker bloom text */}
@@ -61,22 +68,22 @@ const Landing = () => {
           <div className="pillar">
             <div className="how-title">Choose a pillar</div>
             <div className="pillar-img-container">
-              <div className="box">
+              <div className="box1">
                 <div className="pillar-img">
                   <img src={food} />
                 </div>
               </div>
-              <div className="box">
+              <div className="box1">
                 <div className="pillar-img">
                   <img src={exercise} />
                 </div>
               </div>
-              <div className="box">
+              <div className="box1">
                 <div className="pillar-img">
                   <img src={relax} />
                 </div>
               </div>
-              <div className="box">
+              <div className="box1">
                 <div className="pillar-img">
                   <img src={mental} />
                 </div>
@@ -87,19 +94,16 @@ const Landing = () => {
             <div className="how-title">Track your progress</div>
             <img src={tracker} />
           </div>
-          <div className="provider">
-            <div className="how-title">Lean on the professionals</div>
-            <img src={support} />
-          </div>
           <div className="community">
             <div className="how-title">Connect with the community</div>
             <img src={community} />
           </div>
+          <div className="provider">
+            <div className="how-title">Lean on the professionals</div>
+            <img src={support} />
+          </div>
         </section>
-        Hello world
-        <nav>
-          <Link to="/">Home</Link> | <Link to="signup">Signup</Link>
-        </nav>
+        <Footer />
       </div>
     </div>
   );
