c9012dd7963fbe3efb883535a3c7b154a776abbd
lizmery <l.vigil096@gmail.com>
2021-11-24 15:11:22 -0800

    Update testimonials content and styling

--- src/pages/aboutComponents/Testimonials.jsx
+++ src/pages/aboutComponents/Testimonials.jsx
@@ -18,6 +18,7 @@ const Testimonials = () => {
                         <div className="testimonial-quote">
                             <q>My partner and I found our therapist through Mohala. After 2 
                             weeks, our relationship improved immensely and we just got started!</q>
+                            <p className="testimonials-name">- Zahra</p>
                         </div>
                     </div>
                     <div className="testimonials-col">
@@ -28,6 +29,7 @@ const Testimonials = () => {
                             <q>Fantastic educational content, achievable goals, and good support. 
                                 It psychologically prepares you for a lifetime of holistic, healthy 
                                 habits, and helps you recenter your mind & body.</q>
+                            <p className="testimonials-name">- Melissa</p>
                         </div>
                     </div>
                     <div className="testimonials-col">
@@ -37,15 +39,17 @@ const Testimonials = () => {
                         <div className="testimonial-quote">
                             <q>Meals, tips, exercise, and meditation all in one place. Everything 
                             I need to stay accountable.</q>
+                            <p className="testimonials-name">- Tara</p>
                         </div>
                     </div>
                 </div>
             </Container>
             <Container className="bottom-container">
-                Holistic living isn’t one size fits all. We are your resource to help you create a lifestyle 
-                that nourishes you as a whole and build a holistic lifestyle by approaching all the four pillars 
-                (Nutrition, Fitness, Rest, Mind) that are interconnected. <br />
-                We help you discover ways and connect you with resources to make lasting lifestyle changes
+                <p>
+                    Holistic living isn’t one size fits all. We want to be your resource to help you create a 
+                    lifestyle that nourishes you as a whole. We help you create a holistic lifestyle by approaching 
+                    all the four pillars (Nutrition, Fitness, Rest, Mind) that are interconnected. 
+                </p>
                 <a href="signup">
                     <Button className="join-btn btn-block button-class">
                         Join Today
