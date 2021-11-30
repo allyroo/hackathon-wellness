77f8c890546c8d433c1ec2a9eff74e32378b30e7
Sujung Choi <schoi0210@gmail.com>
2021-11-29 16:53:49 -0800

    added flower icon to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom';
 import {Navbar, NavbarBrand} from 'reactstrap';
 import {TiSocialInstagram, TiSocialFacebook, TiSocialTwitter} from 'react-icons/ti';
+import { IoFlowerOutline } from "react-icons/io5";
 
 import './Footer.css';
 
 const Footer = () => {
     return (
         <Navbar fixed="bottom" className="footer">
-            <NavbarBrand href="/">
-                <h3>Mohala</h3>
+            <NavbarBrand href="#top">
+                <h3>Mohala <IoFlowerOutline /></h3>
             </NavbarBrand>
             <div>
                 <Link to="/">
