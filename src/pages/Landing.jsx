06ef87de112cc989b734f0a69ad1d569d9ab6f71
allyroo <allyse.arucan@gmail.com>
2021-11-29 19:35:50 -0700

    landing responsiveness + content changes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -15,40 +15,36 @@ import Footer from "../components/Footer";
 import { IoIosArrowDown } from "react-icons/io";
 import { IoFlowerOutline } from "react-icons/io5";
 import { NavLink, Button } from "reactstrap";
-
 import "./Landing.css";
-
 const Landing = () => {
   return (
     <div>
       <NavBar />
-
-      <div style={{ textAlign: "center" }}>
+      <main className="hero-wrapper">
         {/* Hero */}
         <section className="hero">
           <div className="hero-overlay">
             <div className="hero-container">
               <div className="hero-left"></div>
               <div className="hero-right">
-                <div className="hero-title">Live an Empowered Life</div>
-                
+                <div className="hero-title">THE EMPOWERED YOU</div>
+
                 <div className="hero-subtext">
-                  Create a personalized plan to track your pillars of wellness.
+                  Create a personalized plan to track
+                  your&nbsp;pillars&nbsp;of&nbsp;wellness.
                 </div>
-                {/* <NavLink href="/about">
+                <NavLink href="/about">
                   <Button>Learn More</Button>
-                </NavLink> */}
+                </NavLink>
               </div>
             </div>
-
             <div className="main" id="section1">
-              <a href="#section2">
+              <a href="#section2" className="arrow-down">
                 <IoIosArrowDown />
               </a>
             </div>
           </div>
         </section>
-
         {/* Who We Are */}
         <div className="main" id="section2">
           <a href="#section1"></a>
@@ -56,7 +52,7 @@ const Landing = () => {
         <section className="product">
           <div className="product-text">
             <div className="product-head">
-              Your one-stop shop improved wellness.
+              Your one-stop shop for improved wellness.
             </div>
             <br />
             <p>
@@ -69,18 +65,18 @@ const Landing = () => {
             <img className="dash-img" src={dashboard} />
           </div>
         </section>
-
         {/* Second feature */}
         <section className="definition">
           <div className="mohala">mo•hala</div>
           <div className="mohala-sub">[mo-HAH-lah] Hawaiian</div>
           <div className="mohala-sub-def">
-            to unfold or blossom, like the petals of a flower would
+            • to unfold or blossom,
+            like&nbsp;the&nbsp;petals&nbsp;of&nbsp;a&nbsp;flower&nbsp;would
+            <br />• the metaphorical unfolding of an idea or initial plan which
+            then evolves into a fully realized vision
           </div>
         </section>
-
         {/* Third feature */}
-
         <section className="product">
           <div className="product-img">
             <img className="mobile-img" src={mobile} />
@@ -96,61 +92,77 @@ const Landing = () => {
             </p>
           </div>
         </section>
-
         <section className="how-to">
           {/* <div className="categories"> */}
-
           <div className="pillar">
             <div className="how-title">
               Dashboards built with your vision in mind.
             </div>
-            <hr class="solid" />
             <div className="pillar-img-container">
-              <div className="tracker-img">
-                <img src={tracker} />
+              <div className="pillar-column">
+                <div className="pillar-circle">
+                  <img src={tracker} className="pillar-img" />
+                </div>
                 <div className="how-subtitle">Track Progress</div>
+                <div className="how-description">
+                  We help you create life changing habits by setting up SMART
+                  goals. Our dashboard gives you a visual look into your daily
+                  goals that helps you to consciously work towards them.
+                </div>
               </div>
 
-              <div className="community-img">
-                <img src={community} />
+              <div className="pillar-column">
+                <div className="pillar-circle">
+                  <img src={community} className="pillar-img" />
+                </div>
                 <div className="how-subtitle">Connect with Community</div>
+                <div className="how-description">
+                  We want to remind you that you are not alone. The power of
+                  community to create wellness is far greater than any
+                  physician, clinic, or hospital.
+                </div>
               </div>
 
-              <div className="support-img">
-                <img src={support} />
-                <div className="how-subtitle">Receive Guidance</div>
+              <div className="pillar-column">
+                <div className="pillar-circle">
+                  <img src={support} className="pillar-img" />
+                </div>
+                <div className="how-subtitle">Find Local Resources</div>
+                <div className="how-description">
+                  We know that help is not a one-size-fits-all fit. We will
+                  navigate local resources for you and help you find the best
+                  fit to your needs to move forward.
+                </div>
               </div>
             </div>
-            <hr class="solid" />
           </div>
 
           {/* <section className="bloom">
-            <div className="title">
-              Dig at the roots instead of hacking at the leaves.{" "}
-            </div>
-          </section> */}
+              <div className="title">
+                Dig at the roots instead of hacking at the leaves.{" "}
+              </div>
+            </section> */}
         </section>
         {/* End Section */}
         <section className="tail">
           <div className="tail-title">
-            "The flower of health blooms when all parts work together."
+            <div className="tail-title-text">
+              "The flower of health blooms when all parts work together."
+            </div>
             <NavLink href="/signup">
               <Button className="button-class" outline>
                 BLOOM TODAY
               </Button>
             </NavLink>
           </div>
-
           {/* <div className="tail-subtext">
-            We help you create a holistic lifestyle by approaching all the four
-            pillars (Mind, Nutrition, Physical, Rest) that are interconnected.{" "}
-          </div> */}
+              We help you create a holistic lifestyle by approaching all the four
+              pillars (Mind, Nutrition, Physical, Rest) that are interconnected.{" "}
+            </div> */}
         </section>
-
         <Footer />
-      </div>
+      </main>
     </div>
   );
 };
-
 export default Landing;
--- src/pages/aboutComponents/Testimonials.jsx
+++ src/pages/aboutComponents/Testimonials.jsx
@@ -1,63 +1,72 @@
-import React from 'react';
-import { Container, Row, Col, Button } from 'reactstrap';
-import woman2 from '../../components/images/women_02.jpg';
-import woman4 from '../../components/images/woman_04.jpg';
-import woman7 from '../../components/images/women_07.jpg';
-import './Testimonials.css';
+import React from "react";
+import { Container, Button } from "reactstrap";
+import woman2 from "../../components/images/women_02.jpg";
+import woman4 from "../../components/images/woman_04.jpg";
+import woman7 from "../../components/images/women_07.jpg";
+import "./Testimonials.css";
 
 const Testimonials = () => {
-    return (
-        <>
-            <Container fluid className="mt-2 p-5 testimonials-container">
-                <h1 className="text-center heading">Testimonials</h1>
-                <div className="testimonials-row">
-                    <div className="testimonials-col">
-                        <div className="testimonials-img-container">
-                            <img src={woman2} alt=""  className="testimonials-img"/>
-                        </div>
-                        <div className="testimonial-quote">
-                            <q>My partner and I found our therapist through Mohala. After 2 
-                            weeks, our relationship improved immensely and we just got started!</q>
-                            <p className="testimonials-name">- Zahra</p>
-                        </div>
-                    </div>
-                    <div className="testimonials-col">
-                        <div className="testimonials-img-container">
-                            <img src={woman4} alt=""  className="testimonials-img"/>
-                        </div>
-                        <div className="testimonial-quote">
-                            <q>Fantastic educational content, achievable goals, and good support. 
-                                It psychologically prepares you for a lifetime of holistic, healthy 
-                                habits, and helps you recenter your mind & body.</q>
-                            <p className="testimonials-name">- Melissa</p>
-                        </div>
-                    </div>
-                    <div className="testimonials-col">
-                        <div className="testimonials-img-container">
-                            <img src={woman7} alt=""  className="testimonials-img"/>
-                        </div>
-                        <div className="testimonial-quote">
-                            <q>Meals, tips, exercise, and meditation all in one place. Everything 
-                            I need to stay accountable.</q>
-                            <p className="testimonials-name">- Tara</p>
-                        </div>
-                    </div>
-                </div>
-            </Container>
-            <Container className="bottom-container">
-                <p>
-                    Holistic living isn’t one size fits all. We want to be your resource to help you create a 
-                    lifestyle that nourishes you as a whole. We help you create a holistic lifestyle by approaching 
-                    all the four pillars (Nutrition, Fitness, Rest, Mind) that are interconnected. 
-                </p>
-                <a href="signup">
-                    <Button className="join-btn btn-block button-class">
-                        Join Today
-                    </Button>
-                </a>
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <Container fluid className="mt-2 p-5 testimonials-container">
+        <h1 className="text-center heading">Testimonials</h1>
+        <div className="testimonials-row">
+          <div className="testimonials-col">
+            <div className="testimonials-img-container">
+              <img src={woman2} alt="" className="testimonials-img" />
+            </div>
+            <div className="testimonial-quote">
+              <q>
+                My partner and I found our therapist through Mohala. After 2
+                weeks, our relationship improved immensely and we just got
+                started!
+              </q>
+              <p className="testimonials-name">- Zahra</p>
+            </div>
+          </div>
+          <div className="testimonials-col">
+            <div className="testimonials-img-container">
+              <img src={woman4} alt="" className="testimonials-img" />
+            </div>
+            <div className="testimonial-quote">
+              <q>
+                Fantastic educational content, achievable goals, and good
+                support. It psychologically prepares you for a lifetime of
+                holistic, healthy habits, and helps you recenter your mind &
+                body.
+              </q>
+              <p className="testimonials-name">- Melissa</p>
+            </div>
+          </div>
+          <div className="testimonials-col">
+            <div className="testimonials-img-container">
+              <img src={woman7} alt="" className="testimonials-img" />
+            </div>
+            <div className="testimonial-quote">
+              <q>
+                Meals, tips, exercise, and meditation all in one place.
+                Everything I need to stay accountable.
+              </q>
+              <p className="testimonials-name">- Tara</p>
+            </div>
+          </div>
+        </div>
+      </Container>
+      <Container className="bottom-container">
+        <p>
+          Holistic living isn’t one size fits all. We want to be your resource
+          to help you create a lifestyle that nourishes you as a whole. We help
+          you create a holistic lifestyle by approaching all the four pillars
+          (Nutrition, Fitness, Rest, Mind) that are interconnected.
+        </p>
+        <a href="signup">
+          <Button className="join-btn btn-block button-class">
+            Join Today
+          </Button>
+        </a>
+      </Container>
+    </>
+  );
 };
 
 export default Testimonials;
